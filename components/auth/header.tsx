50c26948ae2aa5e49bd270cc69887b610a43a8c9
Burhanburak <burhanburakozcaan@gmail.com>
2024-09-12 18:07:43 +0300

    password-r

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -8,9 +8,9 @@ const Header = ({ label }: HeaderProps) => {
   return (
     <div className="w-full flex flex-col gap-y-4 items-center justify-center">
       <div className="flex items-center justify-center">
-        <MdOutlineSecurity className="text-sky-400 text-3xl" />
+        <MdOutlineSecurity className="text-red-600 text-3xl" />
         <h1 className="text-3xl font-semibold text-primary drop-shadow-md">
-          Auth
+          Fii
         </h1>
       </div>
 
